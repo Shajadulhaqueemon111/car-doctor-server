9688211438b86899d44e1bc8d914196c88294bd3
Md Emon <mdshajdulhaqueemon@gmail.com>
2023-10-26 02:24:36 +0600

    crud operation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors=require('cors')
 require('dotenv').config()
 const app=express();
@@ -31,15 +31,59 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const serViceCollection=client.db('CarsDoctors').collection('services')
+
+    const bookingCollection=client.db('CarsDoctors').collection('bookings')
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+    app.get('/services',async(req,res)=>{
+        const cursor=serViceCollection.find()
+        const result=await cursor.toArray()
+        res.send(result)
+    })
+
+    app.get('/services/:id',async(req,res)=>{
+        const id=req.params.id;
+        const query={_id:new ObjectId(id)}
+        const options = {
+            // Sort matched documents in descending order by rating
+           
+            // Include only the `title` and `imdb` fields in the returned document
+            projection: {title: 1, price: 1,service_id:1,img:1 },
+          };
+        const result=await serViceCollection.findOne(query,options)
+        res.send(result)
+    })
+
+    //booking related
+
+    app.get('/bookings',async(req,res)=>{
+        console.log(req.query.email)
+        let query={};
+        if(req.query?.email){
+            query={email:req.query.email}
+
+        }
+
+        const result=await bookingCollection.find(query).toArray()
+        res.send(result)
+    })
+
+    app.post('/bookings',async(req,res)=>{
+        const booking=req.body;
+       console.log(booking)
+       const result=await bookingCollection.insertOne(booking)
+       res.send(result)
+    })
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
-run().catch(console.dir);
+run().catch(console.dir); 
 
 
 app.get('/', (req, res) => {
